d6cca6b515922fc7f5d97567927928da2c8665b6
aashish46ak-lab <aashish46ak@gmail.com>
2026-08-01 06:40:51 +0545

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { registerPWA } from "./pwa";
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({
+  immediate: true,
+  onNeedRefresh() {
+    window.location.reload();
+  },
+  onOfflineReady() {
+    console.log("App ready for offline use");
+  },
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
-
-registerPWA();
-
